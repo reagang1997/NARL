b9857be8cea730fa3e1b56f445a09d15257bace6
reagang1997 <73358475+reagang1997@users.noreply.github.com>
2021-01-06 11:43:05 -0600

    Update practice.js

--- assets/js/practice.js
+++ assets/js/practice.js
@@ -1,3 +1,3 @@
-var $practiceResult = $.getJSON("assets\\data\\cota\\2021_1_5_13_21_PRACTICE.json", function(result){
+var $practiceResult = $.getJSON("file:\\assets\\data\\cota\\2021_1_5_13_21_PRACTICE.json", function(result){
     console.log(result);
 })
